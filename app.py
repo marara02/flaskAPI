c84ccd24f888e18c591c044c6c8ab36bafd48ccc
Marara <57908460+marara02@users.noreply.github.com>
2022-05-23 14:04:35 +0600

    make it better

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+import pandas as pd
 from flask_sqlalchemy import SQLAlchemy
 
 model = pickle.load(open('model.pkl', 'rb'))
@@ -41,6 +42,55 @@ class SensorValues(db.Model, JsonModel):
         self.GyroZ = GyroZ
 
 
+class DriverRates(db.Model, JsonModel):
+    __tablename__ = 'driver_rate'
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime)
+    driving_id = db.Column(db.Integer)
+    acceleration_rate = db.Column(db.Integer)
+    braking_rate = db.Column(db.Integer)
+    cornering_rate = db.Column(db.Integer)
+    safety_score = db.Column(db.Integer)
+
+    def __init__(self, timestamp, driving_id, acceleration_rate, braking_rate, cornering_rate, safety_score):
+        self.timestamp = timestamp
+        self.driving_id = driving_id
+        self.acceleration_rate = acceleration_rate
+        self.braking_rate = braking_rate
+        self.cornering_rate = cornering_rate
+        self.safety_score = safety_score
+
+
+class SensorValuesWithTarget(db.Model, JsonModel):
+    __tablename__ = 'sensor_data_target'
+    id = db.Column(db.Integer, primary_key=True)
+    AccX = db.Column(db.Float)
+    AccY = db.Column(db.Float)
+    AccZ = db.Column(db.Float)
+    GPS_Long = db.Column(db.Float)
+    GPS_Lat = db.Column(db.Float)
+    GyroX = db.Column(db.Float)
+    GyroY = db.Column(db.Float)
+    GyroZ = db.Column(db.Float)
+    Target = db.Column(db.Integer)
+
+    def __init__(self, AccX, AccY, AccZ, GPS_Long, GPS_Lat, GyroX, GyroY, GyroZ, Target):
+        self.AccX = AccX
+        self.AccY = AccY
+        self.AccZ = AccZ
+        self.GPS_Long = GPS_Long
+        self.GPS_Lat = GPS_Lat
+        self.GyroX = GyroX
+        self.GyroY = GyroY
+        self.GyroZ = GyroZ
+        self.Target = Target
+
+
+@app.route('/')
+def home():
+    return "Hello World"
+
+
 @app.route('/saveData', methods=['POST'])
 def save_data():
     if request.method == 'POST':
@@ -64,9 +114,30 @@ def get_data():
     return json.dumps([ss.as_dict() for ss in SensorValues.query.all()])
 
 
-@app.route('/')
-def home():
-    return "Hello World"
+@app.route('/getResult', methods=['GET'])
+def get_result():
+    js = pd.read_json(json.dumps([ss.as_dict() for ss in SensorValues.query.all()]))
+    result = model.predict(js)
+    dst = []
+    new_result = []
+    for i in result:
+        new_result.append(i)
+    acceleration_times = [x for x in new_result if x == 0]
+    braking_times = [x for x in new_result if x == 1]
+    cornering_times = [x for x in new_result if x == 2]
+    acceleration_rate = len(acceleration_times)
+    braking_rate = len(braking_times)
+    cornering_rate = len(cornering_times)
+    return jsonify({'acceleration_rate': acceleration_rate,
+                    'braking_rate': braking_rate,
+                    'cornering_rate': cornering_rate})
+
+
+@app.route('/sendEndResult', methods=['POST'])
+def send_end_result_of_driver():
+    dct = get_result()
+    print(dct[1])
+    return get_result()
 
 
 @app.route('/predict', methods=['POST'])
@@ -80,11 +151,6 @@ def predict():
     input_query = np.array([[GyroX, GyroY, GyroZ, AccX, AccY, AccZ]])
     result = model.predict(input_query)[0]
     return jsonify({'Y': str(result)})
-
-
-@app.route('/lol')
-def home_1():
-    return "Hello World"
 
 
 if __name__ == '__main__':
